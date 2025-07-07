797fffeadf559c3aeafc352b2dc6b40e447edf21
fahmi <amixcustomlinux@gmail.com>
2025-07-07 10:34:17 +0700

    [dev] test hardcoded redirect

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    if (!process.env.REDIRECTS_DATA) {
+      console.warn("REDIRECTS_DATA environment variable is not set.");
+      return [];
+    }
+
+    try {
+      const redirectsData = JSON.parse(process.env.REDIRECTS_DATA);
+      return redirectsData.map(item => ({
+        source: item.source,
+        destination: item.destination,
+        permanent: true,
+      }));
+    } catch (error) {
+      console.error("Failed to parse REDIRECTS_DATA:", error);
+      return [];
+    }
+  },
 };
 
 export default nextConfig;
